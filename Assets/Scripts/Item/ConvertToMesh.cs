92f277cb22e23392310d3d5686fe924e3ac74c5a
Ky2016z <anhky2016z@gmail.com>
2017-10-31 17:21:41 +0700

    Fix map

--- Assets/Scripts/Item/ConvertToMesh.cs
+++ Assets/Scripts/Item/ConvertToMesh.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConvertToMesh : MonoBehaviour {
+public class ConvertToMesh : MonoBehaviour
+{
 
-   [ContextMenu("Convert to regular mesh")]
-   void Convert()
+    [ContextMenu("Convert to regular mesh")]
+    void Convert()
     {
         SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
         MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
